ee8bbb3864ef5c7f274e7f31e347167e0828bc0e
JPmurage <princemurage@gmail.com>
2026-03-16 21:31:04 +0300

    Unecessary file removed

--- try1.js
+++
@@ -1,9 +0,0 @@
-function randomCOl(){
-
-let r= Math.round(Math.random()*256);
-let g=Math.round(Math.random()*256);
-let b=Math.round(Math.random()*256);
-
-document.body.style.backgroundColor=` rgb(${r},${g},${b})`
-
-}
